3d414d3c2a714fd69ee07a8e82ea676df503cca9
ウェンベル <wenvelk@gmail.com>
2025-03-13 08:33:41 -0300

    Add files via upload

---
+++ number par C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números pares de 1 a 100:");
+
+        for (int i = 2; i <= 100; i += 2)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine(); 
+    }
+}
